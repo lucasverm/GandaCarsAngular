e0ac47ecd2ff4f8eae1897a91935416de50e7864
lucasverm <lucasvermeulen@gmail.com>
2020-04-11 18:29:44 +0200

    nieuwe start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { BusChauffeurOverzichtComponent } from './bus-chauffeur-overzicht/bus-chauffeur-overzicht.component';
 import { BuschauffeurInfoComponent } from './buschauffeur-info/buschauffeur-info.component';
 import { BusChauffeurDetailsComponent } from './bus-chauffeur-details/bus-chauffeur-details.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { dienstKalenderComponent } from './dienst-kalender/dienst-kalender.component';
-
-import {
-  CalendarModule, DateAdapter, CalendarDateFormatter,
-  CalendarMomentDateFormatter,
-  MOMENT,
-} from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { CommonModule } from '@angular/common';
-import { registerLocaleData } from '@angular/common';
-import localeBe from '@angular/common/locales/nl-BE';
-import localeBeExtra from '@angular/common/locales/extra/nl-BE';
 import { DienstOverzichtComponent } from './dienst-overzicht/dienst-overzicht.component';
 import { DienstInfoComponent } from './dienst-info/dienst-info.component';
 
-import moment from 'moment-timezone';
-registerLocaleData(localeBe, 'nl-BE', localeBeExtra);
 
 @NgModule({
   declarations: [
@@ -44,26 +30,9 @@ registerLocaleData(localeBe, 'nl-BE', localeBeExtra);
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
-
-    CommonModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    },
-      {
-        dateFormatter: {
-          provide: CalendarDateFormatter,
-          useClass: CalendarMomentDateFormatter,
-        },
-      }),
-    BrowserAnimationsModule
-
+    HttpClientModule
   ],
-  providers: [{
-    provide: MOMENT,
-    useValue: moment,
-  },],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dienst-kalender/dienst-kalender.component.ts
+++ src/app/dienst-kalender/dienst-kalender.component.ts
@@ -1,34 +1,8 @@
 import {
   Component,
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Input,
+  Input
 } from '@angular/core';
-import RRule from 'rrule';
-import moment from 'moment-timezone';
-import {
-  CalendarDayViewBeforeRenderEvent,
-  CalendarEvent,
-  CalendarMonthViewBeforeRenderEvent,
-  CalendarView,
-  DAYS_OF_WEEK,
-  CalendarWeekViewBeforeRenderEvent,
-} from 'angular-calendar';
-import { ViewPeriod } from 'calendar-utils';
 import { Dienst } from '../modals/dienst';
-
-interface RecurringEvent {
-  title: string;
-  color: any;
-  rrule?: {
-    freq: any;
-    bymonth?: number;
-    bymonthday?: number;
-    byweekday?: any;
-  };
-}
-
-moment.tz.setDefault('Utc');
 
 @Component({
   selector: 'app-dienst-kalender',
@@ -37,60 +11,8 @@ moment.tz.setDefault('Utc');
 })
 export class dienstKalenderComponent {
   @Input() diensten: Dienst[];
-  view: CalendarView = CalendarView.Week;
-  locale: string = 'nl-BE';
-  viewDate = moment().toDate();
-  calendarEvents: CalendarEvent[] = [];
-  viewPeriod: ViewPeriod;
-  constructor(private cdr: ChangeDetectorRef) { }
+  
+  constructor() { }
 
-  updateCalendarEvents(
-    viewRender:
-      | CalendarMonthViewBeforeRenderEvent
-      | CalendarWeekViewBeforeRenderEvent
-      | CalendarDayViewBeforeRenderEvent
-  ): void {
-    if (
-      !this.viewPeriod ||
-      !moment(this.viewPeriod.start).isSame(viewRender.period.start) ||
-      !moment(this.viewPeriod.end).isSame(viewRender.period.end)
-    ) {
-      this.viewPeriod = viewRender.period;
-      console.log(viewRender.period);
-      
-      this.calendarEvents = [];
-      let eventen = [
-        {
-          title: 'Recurs weekly on mondays',
-          color: "red",
-
-          rrule: {
-            freq: RRule.WEEKLY,
-            byweekday: [RRule.MO],
-          },
-        },
-      ];
-      eventen.forEach((event) => {
-        const rule: RRule = new RRule({
-          ...event.rrule,
-          dtstart: moment(viewRender.period.start).startOf('day').toDate(),
-          until: moment(viewRender.period.end).endOf('day').toDate(),
-        });
-        const { title, color } = event;
-
-        rule.all().forEach((date) => {
-          this.calendarEvents.push({
-            title,
-            color,
-            start: moment(date).toDate(),
-          });
-        });
-      });
-      this.cdr.detectChanges();
-    }
-  }
-
-  eventClicked({ event }: { event: CalendarEvent }): void {
-    console.log('Event clicked', event);
-  }
+  
 }
